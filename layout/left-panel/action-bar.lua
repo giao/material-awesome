2aa0788cbc180009bfe540f31e6d3f5cecfdb515
giao@TP470 <giao.dinh@gmail.com>
2020-11-08 18:45:30 +0100

    cleanup

--- layout/left-panel/action-bar.lua
+++ layout/left-panel/action-bar.lua
@@ -90,6 +90,7 @@ return function(screen, panel, action_bar_width)
       -- Right widgets
       layout = wibox.layout.fixed.vertical,
       wibox.container.margin(systray, dpi(10), dpi(10)),
+			--mykeyboardlayout,
       require('widget.package-updater'),
       --require('widget.wifi'),
       require('widget.battery'),

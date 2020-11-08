511f9d4a0ad03c38bf18fb976b414a3d06212d2e
giao@TP470 <giao.dinh@gmail.com>
2020-11-08 09:08:07 +0100

    customized for apps on TP470

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -3,7 +3,7 @@ local filesystem = require('gears.filesystem')
 -- Thanks to jo148 on github for making rofi dpi aware!
 local with_dpi = require('beautiful').xresources.apply_dpi
 local get_dpi = require('beautiful').xresources.get_dpi
-local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi -run-command "/bin/bash -c -i \'shopt -s expand_aliases; {cmd}\'"'
+local rofi_command = 'env /usr/bin/rofi -modi "window,run,ssh,drun" -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi -run-command "/bin/bash -c -i \'shopt -s expand_aliases; {cmd}\'"'
 
 return {
   -- List of apps to start by default on some actions
@@ -11,31 +11,32 @@ return {
     terminal = 'terminator',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'terminator',
-    screenshot = 'flameshot full -p ~/Pictures',
-    region_screenshot = 'flameshot gui -p ~/Pictures',
-    delayed_screenshot = 'flameshot full -p ~/Pictures -d 5000',
-    browser = 'brave',
-    editor = 'gedit', -- gui text editor
+    quake = 'st',
+    screenshot = 'flameshot full -p ~/Pictures/screenshots',
+    region_screenshot = 'flameshot gui -p ~/Pictures/screenshots',
+    delayed_screenshot = 'flameshot full -p ~/Pictures/screenshots -d 5000',
+    browser = 'waterfox-current',
+    editor = 'xed', -- gui text editor
     social = 'discord',
     game = rofi_command,
-    files = 'nautilus',
-    music = rofi_command 
+    files = 'nemo',
+    music = rofi_command
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
     'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
     'nm-applet --indicator', -- wifi
     'pnmixer', -- shows an audiocontrol applet in systray when installed.
-    --'blueberry-tray', -- Bluetooth tray icon
+    'blueberry-tray', -- Bluetooth tray icon
     'numlockx on', -- enable numlock
     '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'xfce4-power-manager', -- Power manager
      'flameshot',
-     'synology-drive -minimized',
+     -- 'synology-drive -minimized',
      'steam -silent',
-     '/usr/bin/barrier',
+     -- '/usr/bin/barrier',
      '~/.local/bin/wallpaper', -- wallpaper-reddit script
+		 '/usr/bin/remmina -i',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
